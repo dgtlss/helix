23280160f0c5a1a70e4dcb1c60939d340c0f65fe
Nathan Langer <32520453+dgtlss@users.noreply.github.com>
2024-03-13 00:33:29 +0000

    Base format

---
+++ src/ServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+    namespace Dgtlss\Helix;
+
+    require_once __DIR__.'/helpers.php';
+
+
+    class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+
+
+        public function boot()
+        {
+            $this->setupConfig(); // Load config
+            if ($this->app->runningInConsole()) {
+                $this->commands([
+                    Commands\Init::class,
+                ]);
+            }
+        }
+
+        public function register()
+        {            
+            // Not needed for this package but you can use this to register your services
+
+        }
+
+        protected function setupConfig(){
+
+            $configPath = __DIR__ . '/../config/helix.php';
+            $this->publishes([$configPath => $this->getConfigPath()], 'config');
+    
+        }
+
+        protected function getConfigPath()
+        {
+            return config_path('helix.php');
+        }
+
+        protected function publishConfig($configPath)
+        {
+            $this->publishes([$configPath => config_path('helix.php')], 'config');
+        }
+
+
+    }
